858b05b5e83c807345c4dc5e5b9daf592372c1d3
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-04 17:25:30 +0530

    logo change and styles modified

--- components/header/navBar/logo.tsx
+++ components/header/navBar/logo.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import React from "react";
 
-import LogoImage from "@/public/images/sant-sport-logo.svg";
+import LogoImage from "@/public/images/sant-sports-logo.png";
 
 const Logo = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center">
       <Image
         src={LogoImage}
         alt="Sant-sport-logo"
@@ -14,6 +14,9 @@ const Logo = () => {
         width={50}
         height={50}
       />
+      <span className="px-2 md:block hidden font-bold text-2xl text-white  hover:text-blue-400">
+        SantSports
+      </span>
     </div>
   );
 };
