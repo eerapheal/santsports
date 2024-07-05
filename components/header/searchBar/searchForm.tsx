d43863e31bf49af8ffc1ef6cae66af50167659d4
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-05 06:48:07 +0530

    filtered teamData

--- components/header/searchBar/searchForm.tsx
+++ components/header/searchBar/searchForm.tsx
@@ -6,10 +6,21 @@ const SearchForm = ({ teamData }: { teamData: Team[] }) => {
   const [focusedIndex, setFocusedIndex] = useState(-1);
   const [showFilteredBox, setShowFilteredBox] = useState(false);
 
+  const filteredTeams = teamData.filter((team) =>
+    team.team.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+    setFocusedIndex(-1);
+    setShowFilteredBox(true);
+  };
+
   return (
     <div>
       <input
         type="text"
+
         placeholder="Search Here"
         className="w-full bg-gradient-to-r from-neutral-100/5 to-black/25
     bg-transparent p-2 outline-none border-neutral-100/00 border-[1px]
