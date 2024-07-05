4226d2a0d1953e295d09fd44d729a1f73f6e3043
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-05 20:22:18 +0530

    handle clicks

--- components/header/searchBar/searchForm.tsx
+++ components/header/searchBar/searchForm.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Team } from "@/utils/type";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const SearchForm = ({ teamData }: { teamData: Team[] }) => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -42,8 +44,30 @@ const SearchForm = ({ teamData }: { teamData: Team[] }) => {
       }
     }
   };
+
+  const handleTeamItemClick = () => {
+    setSearchTerm("");
+  };
+  const teamListRef = useRef<HTMLDivElement>(null);
+
+  const handleOutSideClick = (event: MouseEvent) => {
+    if (
+      teamListRef.current &&
+      !teamListRef.current.contains(event.target as Node)
+    ) {
+      setShowFilteredBox(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOutSideClick);
+    return () => {
+      document.removeEventListener("click", handleOutSideClick);
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="relative">
       <input
         type="text"
         value={searchTerm}
@@ -52,9 +76,24 @@ const SearchForm = ({ teamData }: { teamData: Team[] }) => {
         placeholder="Search Here"
         className="w-full bg-gradient-to-r from-neutral-100/5 to-black/25
     bg-transparent p-2 outline-none border-neutral-100/00 border-[1px]
-    rounded-xl hover:border-blue-400 focus:border-blue-400/10 text-neutral-100 placeholder:text-neutral-100/00
-    "
+    rounded-xl hover:border-blue-400 focus:border-blue-400/10 text-neutral-100 placeholder:text-neutral-100/00"
       />
+      {searchTerm && filteredTeams.length > 0 && showFilteredBox && (
+        <div className="absolute border-[1px] border-blue-400 hover:border-none rounded-xl left-0 w-full max-w-md bg-black/80 z-20 flex flex-col">
+          {filteredTeams.slice(0, 10).map((standing, i) => (
+            <Link
+              href={`/team/${standing.team.id}`}
+              key={standing.team.id}
+              className={`p-2 text-neutral-100 ${
+                i === focusedIndex ? "bg-neutral-100/40" : ""
+              }`}
+              onClick={handleTeamItemClick}
+            >
+              {standing.team.name}{" "}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
