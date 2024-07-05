6772d8954be1c5c1443f51cd78eedfd695191126
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-05 20:19:57 +0530

    team data props used

--- components/header/navBar/navBar.tsx
+++ components/header/navBar/navBar.tsx
@@ -4,8 +4,11 @@ import Logo from "./logo";
 import NavItems from "./navItems";
 import Link from "next/link";
 import SearchForm from "../searchBar/searchForm";
+import { Team } from "@/utils/type";
+import getTeams from "@/utils/getApis/getTeams";
 
-const NavBar = () => {
+const NavBar = async () => {
+  let teamData: Team[] = await getTeams();
   return (
     <div className="flex text-white justify-center items-center w-full p-3 bg-black/40">
       <div className="w-1/6 flex justify-center items-center">
@@ -14,7 +17,7 @@ const NavBar = () => {
         </Link>
       </div>
       <div className="w-4/6 flex justify-center items-center">
-        <SearchForm />
+        <SearchForm teamData={teamData} />
       </div>
       <div className="w-1/6">
         <NavItems />
