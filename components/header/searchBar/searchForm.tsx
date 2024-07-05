45db748ba699eef84f16f1b722f691b25bc6d513
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-05 07:33:15 +0530

    teams filtered with onKeyDown function

--- components/header/searchBar/searchForm.tsx
+++ components/header/searchBar/searchForm.tsx
@@ -1,10 +1,13 @@
 import { Team } from "@/utils/type";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const SearchForm = ({ teamData }: { teamData: Team[] }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [focusedIndex, setFocusedIndex] = useState(-1);
   const [showFilteredBox, setShowFilteredBox] = useState(false);
+
+  let router = useRouter();
 
   const filteredTeams = teamData.filter((team) =>
     team.team.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -16,12 +19,36 @@ const SearchForm = ({ teamData }: { teamData: Team[] }) => {
     setShowFilteredBox(true);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "ArrowDown") {
+      let length = 0;
+      if (filteredTeams.length > 10) {
+        length = 10;
+      } else {
+        length = filteredTeams.length;
+      }
+      setFocusedIndex((prevIndex) =>
+        prevIndex < length - 1 ? prevIndex - 1 : prevIndex
+      );
+    } else if (event.key === "ArrowUp") {
+      event.preventDefault();
+      setFocusedIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : prevIndex
+      );
+    } else if (event.key === "Enter") {
+      if (focusedIndex !== -1) {
+        const teamId = filteredTeams[focusedIndex].team.id;
+        router.push(`/team/${teamId}`);
+      }
+    }
+  };
   return (
     <div>
       <input
         type="text"
         value={searchTerm}
         onChange={handleSearchChange}
+        onKeyDown={handleKeyDown}
         placeholder="Search Here"
         className="w-full bg-gradient-to-r from-neutral-100/5 to-black/25
     bg-transparent p-2 outline-none border-neutral-100/00 border-[1px]
