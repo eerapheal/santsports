28adde3a264b1b69dded535ca35c719f12893d83
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-06 23:18:12 +0530

    standing league name and active tab

--- components/home/standing.fixtures.tsx
+++ components/home/standing.fixtures.tsx
@@ -1,18 +1,39 @@
 "use client";
 import { Standing } from "@/utils/type";
-import React from "react";
+import React, { useState } from "react";
 
 const StandingFixtures = ({ standingData }: { standingData: Standing[] }) => {
+  const menuItems = ["EPL", "La Liga", "Bundesliga", "Serie A", "Ligue 1"];
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
-    <div className="flex flex-col w-full max-w-7xl bg-gradient-to-br from-blue-700/40 lg:flex-row rounded-xl">
+    <div className="flex flex-col w-full max-w-7xl bg-gradient-to-br from-blue-700 lg:flex-row">
       <div className="flex justify-center items-center lg:w-3/5 md:p-10 py-5">
         <div className="flex flex-col justify-center items-center bg-gradient-to-b from-black/40 w-full text-neutral-100 rounded-xl">
           <div className="w-full flex flex-col justify-center items-center">
             <div className="p-2 font-bold capitalize">Sanding</div>
+            <div className="flex justify-center w-full">
+              {menuItems.map((items, i) => (
+                <button
+                  key={i}
+                  onClick={() => setActiveTab(i)}
+                  className={`w-full p-4 rounded md:text-base text-xs font-bold ${
+                    i === activeTab
+                      ? "text-neutral-100"
+                      : "text-gray-300 bg-black/10"
+                  }`}
+                >
+                  {items}
+                </button>
+              ))}
+            </div>
+            <div className="w-full flex overflow-x-hidden snap-x scrollbar-none scroll-smooth text-xs md:text-sm">
+              Sanding
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex text-neutral-100 font-bold justify-center items-center lg:w-2/5 p-10 lg:pr-5 pb-10 lg:p-0">
+      <div className="flex text-neutral-100 font-bold justify-center items-center lg:w-2/5 p-10 lg:pr-5 pb-10 lg:p-0 ">
         Fixtures
       </div>
     </div>
