cf715b0c454879b2a47221fe16df7bd4bc666752
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-06 21:22:45 +0530

    standing and fixture component render with suspense

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,20 +1,29 @@
 import { Metadata } from "next";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 
+import { Standing } from "@/utils/type";
+import getStandings from "@/utils/getApis/getStanding";
 import Loading from "./loading";
+const StandingFixtures = React.lazy(
+  () => import("@/components/home/standing.fixtures")
+);
 
 export const metadata: Metadata = {
-  title: process.env.NEXT_PUBLIC_APP_NAME || "SantSport live sports scores like never before",
+  title:
+    process.env.NEXT_PUBLIC_APP_NAME ||
+    "SantSport live sports scores like never before",
   description:
     process.env.NEXT_PUBLIC_APP_DESC ||
     "Experience the latest live sports scores like never before with Sant Sport. We are committed to providing you with all the trending sports news and real-time scores on the go.",
 };
 
-const HomePage = () => {
+const HomePage = async () => {
+  const standingData: Standing[] = await getStandings();
   return (
-    <div className="">
-      <Suspense fallback={<Loading />}></Suspense>
-      home
+    <div className="flex flex-col justify-center items-center w-full md:p-10">
+      <Suspense fallback={<Loading />}>
+        <StandingFixtures standingData={standingData} />
+      </Suspense>
     </div>
   );
 };
