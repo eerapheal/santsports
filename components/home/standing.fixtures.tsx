bfec0db89848497590f26e71316d23457910eb6e
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-07 18:18:06 +0530

    complete standing

--- components/home/standing.fixtures.tsx
+++ components/home/standing.fixtures.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Standing } from "@/utils/type";
+import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
 const StandingFixtures = ({ standingData }: { standingData: Standing[] }) => {
@@ -24,7 +25,22 @@ const StandingFixtures = ({ standingData }: { standingData: Standing[] }) => {
     scrollToTab(index);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.shiftKey) {
+        event.preventDefault();
+      }
+    };
+    const container = menuRef.current;
+    if (container) {
+      container.addEventListener("wheel", handleWheel);
+    }
+    return () => {
+      if (container) {
+        container.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
 
   return (
     <div className="flex flex-col w-full max-w-7xl bg-gradient-to-br from-blue-700 lg:flex-row">
@@ -37,7 +53,7 @@ const StandingFixtures = ({ standingData }: { standingData: Standing[] }) => {
                 <button
                   key={i}
                   onClick={() => handleTabClick(i)}
-                  className={`w-full p-4 rounded md:text-base text-xs font-bold ${
+                  className={`w-full p-4 rounded md:text-base font-bold ${
                     i === activeTab
                       ? "text-neutral-100"
                       : "text-gray-300 bg-black/10"
@@ -47,8 +63,96 @@ const StandingFixtures = ({ standingData }: { standingData: Standing[] }) => {
                 </button>
               ))}
             </div>
-            <div className="w-full flex overflow-x-hidden snap-x scrollbar-none scroll-smooth text-xs md:text-sm">
-              Sanding
+            <div
+              ref={menuRef}
+              className="w-full flex overflow-x-hidden snap-x scrollbar-none scroll-smooth text-xs md:text-sm"
+            >
+              {standingData.map((responseData, i) => (
+                <div
+                  key={responseData.league.id}
+                  className="flex-shrink-0 w-full snap-center flex justify-center items-center"
+                >
+                  <div className="flex flex-col justify-between p-2 w-full">
+                    <div className="flex w-full p-1">
+                      <div className="w-1/12">Rank</div>
+                      <div className="w-3/12">Team</div>
+                      <ul className="w-6/12 flex text-sm justify-evenly">
+                        <li className="w-full text-center">M</li>
+                        <li className="w-full text-center">W</li>
+                        <li className="w-full text-center">D</li>
+                        <li className="w-full text-center">L</li>
+                        <li className="w-full text-center font-bold">P</li>
+                        <li className="w-full text-center">GF</li>
+                        <li className="w-full text-center">GA</li>
+                        <li className="w-full text-center">GD</li>
+                      </ul>
+                      <div className="w-2/12 text-center">Form</div>
+                    </div>
+                    {responseData.league.standings[0].map((team, j) => (
+                      <Link
+                        href={`/team/${team.team.id}`}
+                        key={j + team.team.name}
+                        className={`flex w-full p-1 hover:bg-blue-400/40 ${
+                          j % 2 === 0 ? "bg-black/40" : ""
+                        }`}
+                      >
+                        <div className="w-1/12 text-xs flex px-2 justify-center items-center">
+                          {j + 1}
+                        </div>
+                        <div className="w-3/12 text-xs flex items-center">
+                          <img
+                            src={team.team.logo}
+                            alt={team.team.name}
+                            className="h-4 w-4 mr-2"
+                          />
+                          {team.team.name}
+                        </div>
+                        <ul className="w-6/12 flex text-xs justify-center items-center">
+                          <li className="w-full text-center">
+                            {team.all.played}
+                          </li>
+                          <li className="w-full text-center">{team.all.win}</li>
+                          <li className="w-full text-center">
+                            {team.all.draw}
+                          </li>
+                          <li className="w-full text-center">
+                            {team.all.lose}
+                          </li>
+                          <li className="w-full text-center font-bold">
+                            {team.points}
+                          </li>
+                          <li className="w-full text-center">
+                            {team.all.goals.for}
+                          </li>
+                          <li className="w-full text-center">
+                            {team.all.goals.against}
+                          </li>
+                          <li className="w-full text-center">
+                            {team.goalsDiff}
+                          </li>
+                        </ul>
+                        <div className="w-2/12 flex justify-center items-center">
+                          {team.form?.split("").map((char, i) => (
+                            <div
+                              key={char + i}
+                              className={`opacity-80 w-3 h-3 m-[1px] rounded-full
+                                                                        ${
+                                                                          char ===
+                                                                          "L"
+                                                                            ? "bg-red-500"
+                                                                            : char ===
+                                                                              "D"
+                                                                            ? "bg-gray-500"
+                                                                            : "bg-green-500"
+                                                                        }`}
+                            ></div>
+                          ))}
+                        </div>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
