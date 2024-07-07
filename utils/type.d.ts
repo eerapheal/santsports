441c0ceb8a0a744b8f353987d76aa870042a37d5
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-07 19:42:13 +0530

    Fixture database varebles

--- utils/type.d.ts
+++ utils/type.d.ts
@@ -42,4 +42,80 @@ type Games = {
   };
 };
 
-export { Standing , Games, Team, League };
+type FixtureInfo = {
+  id: number;
+  referee: string;
+  timezone: string;
+  date: string;
+  timestamp: number;
+  periods: {
+    first: number;
+    second: number;
+  };
+  venue: {
+    id: number;
+    name: string;
+    city: string;
+  };
+  status: {
+    long: string;
+    short: string;
+    elapsed: number;
+  };
+};
+
+type LeagueFixtures = {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  flag: string;
+  season: number;
+  round: string;
+};
+
+type Teams = {
+  home: {
+    id: number;
+    name: strung;
+    logo: sting;
+    winner: boolean;
+  };
+  away: {
+    id: number;
+    name: strung;
+    logo: sting;
+    winner: boolean;
+  };
+};
+
+type Gaols = {
+  home: number;
+  away: number;
+};
+
+type Score = {
+  halftime: Gaols;
+  fulltime: Gaols;
+  teams: Teams;
+  goals: Gaols;
+  score: Score;
+};
+
+type Fixture = {
+  fixture: FixtureInfo;
+  league: LeagueFixtures;
+  teams: Teams;
+  goals: Gaols;
+  score: ScrollRestoration;
+};
+export {
+  Standing,
+  Games,
+  Team,
+  League,
+  Fixture,
+  Score,
+  fixtureInfo,
+  LeagueFixtures,
+};
