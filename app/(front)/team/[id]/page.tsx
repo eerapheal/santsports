1b229e6d335d8e21f851ab6a0120068f5c2d4b88
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-07 18:16:23 +0530

    styles add

--- app/(front)/team/[id]/page.tsx
+++ app/(front)/team/[id]/page.tsx
@@ -6,7 +6,7 @@ type PageProps = {
   };
 };
 const Page = ({ params }: PageProps) => {
-  return <div>{params.id}</div>;
+  return <div className="flex justify-center items-center text-neutral-100">{params.id}</div>;
 };
 
 export default Page;
