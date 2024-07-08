fd8abae46aa4c882abdf53b933ec688e9dc0ec2d
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-09 02:33:01 +0530

    fixture data fetched

---
+++ utils/getApis/getFixtures.tsx
@@ -0,0 +1,106 @@
+import moment from "moment";
+import { USE_SAMPLE } from "../sampleData/useSampleData"
+import { AllFixtures, Fixture } from "../type";
+import getFixturesSample from "../sampleData/getFixturesSample";
+
+
+const API_KEY = process.env.API_KEY as string;
+
+const leagues = [
+  { league: 19, name: 'EPL' },
+  { league: 149, name: 'La Liga' },
+  { league: 28, name: 'BundesLiga' },
+  { league: 135, name: 'Serie a' },
+  { league: 61, name: 'Ligue 1' },
+  { league: 2, name: 'Champions League' },
+  { league: 3, name: 'Europa League' },
+  { league: 548, name: 'Conference League' },
+  { league: 531, name: 'UEFA Super Cup' },
+  { league: 15, name: 'Fifa Club World Cup' },
+  { league: 45, name: 'FA Cup' },
+  { league: 48, name: 'Carabao Cup' },
+  { league: 528, name: 'Community Shield' },
+  { league: 143, name: 'Copa Del Ray' },
+  { league: 556, name: 'Super Cup LaLiga' },
+  { league: 529, name: 'Super Cup BundesLiga' },
+  { league: 547, name: 'Super Cup Serie A' },
+  { league: 137, name: 'Coppa Italia' },
+  { league: 65, name: 'Coupe De Li Ligue' },
+  { league: 66, name: 'Coupe De France' },
+  { league: 526, name: 'Trophes Des Champoins' },
+]
+
+async function fetchFixturesByLeague(year: number, league: number): Promise<Fixture> {
+  //   const FIXTURE_BY_LEAGUE_ID: string = process.env.FIXTURE_BY_LEAGUE_ID as string;
+  const url = `FIXTURE_BY_LEAGUE_ID = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=${league}&season${year}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    },
+    next: {
+      // validate data every 24 hours
+      revalidate: 60 * 60 * 24,
+    },
+
+  };
+
+  try{
+  const response = await fetch(url, options)
+  const  data = await response.json();
+      const fixtures: Fixture[] = data.response;
+        return fixtures || [];
+      
+    }catch(err) {
+      console.error(`Error fetching ${league} fixtures on year ${year}: ${err}`)
+  return [];
+    }
+}
+export default async function getFixtures(): Promise<AllFixtures[]> {
+  if (USE_SAMPLE) {
+    return getFixturesSample();
+  }
+
+  try {
+    const currentTime = moment();
+    const year = currentTime.year();
+    const month = currentTime.month()
+
+    const allFixturesBgLeague: AllFixtures[] = [];
+
+    for (const league of leagues) {
+      if (month <= 5) {
+        allFixturesBgLeague.push({
+          name: league.name,
+          fixtures: await fetchFixturesByLeague(year - 1, league.league)
+        });
+      } else if (month >= 5) {
+        allFixturesBgLeague.push({
+          name: league.name,
+          fixtures: await fetchFixturesByLeague(year, league.league)
+        });
+      } else {
+        allFixturesBgLeague.push({
+          name: league.name,
+          fixtures: await fetchFixturesByLeague(year - 1, league.league)
+        });
+        const existingData = allFixturesBgLeague.find.((data) =>
+          data.name === league.name
+        );
+        if (existingData) {
+          existingData.fixtures.push(...(await fetchFixturesByLeague(year, league.league)))
+        } else {
+          allFixturesBgLeague.push({
+          name: league.name,
+            fixtures: await fetchFixturesByLeague(year, league.league)
+        })}
+      }
+    }
+
+    return allFixturesBgLeague;
+  } catch (error) {
+    console.error(`Error fetching all fixtures on year, error`)
+  throw error
+  }
+}
