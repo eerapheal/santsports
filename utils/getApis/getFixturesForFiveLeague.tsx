f14d7966de0c72657f498cb14052a6dd544f058c
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-09 04:42:37 +0530

    fetch fixtures for five league matching

---
+++ utils/getApis/getFixturesForFiveLeague.tsx
@@ -0,0 +1,33 @@
+import moment from "moment";
+import { AllFixtures } from "../type";
+import getFixtures from "./getFixtures";
+
+export default async function getFixturesForFiveLeague(): Promise<AllFixtures[]> {
+  try {
+    const allFixturesByLeague = await getFixtures();
+
+    const fixturesForFiveLeagues: AllFixtures[] = []
+    for (const league of allFixturesByLeague) {
+      if (league.name === 'EPL' ||
+        league.name === 'La Liga' ||
+        league.name === 'BundesLiga' ||
+        league.name === 'Serie a' ||
+        league.name === 'Ligue 1') {
+        fixturesForFiveLeagues.push(league)
+      }
+    }
+
+    const filteredFixtures: AllFixtures[] = fixturesForFiveLeagues.filter(
+      (league) => {
+        league.fixtures = league.fixtures.filter((fixture) => {
+          return moment(fixture.fixture.date).isAfter(moment().subtract(1, 'day'), 'day')
+        }).slice(0, 5);
+        return league.fixtures.length > 0;
+      }
+    );
+    return filteredFixtures;
+  } catch (error) {
+    console.error('An error occureed while fetching fixtures', error)
+    throw error;
+  }
+};
