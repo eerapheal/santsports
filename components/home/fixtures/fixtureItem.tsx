a0885fc7b5671c541b1d85c30f7bc85e1ee9536f
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-17 05:33:28 +0530

    fixtures items

---
+++ components/home/fixtures/fixtureItem.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import LocalTime from "@/utils/localTime/localTime";
+import { Fixture } from "@/utils/type";
+import moment from "moment";
+import Image from "next/image";
+import Link from "next/link";
+
+type PageProps = {
+  match: Fixture;
+  index: number;
+};
+
+
+const FixtureItem = ({ match, index }: PageProps) => {
+  const today = moment();
+  const matchDate = moment(match.fixture.date);
+  return today.isBefore(matchDate) ? (
+    <Link
+      href={`/match/${match.fixture.id}`}
+      key={match.fixture.id}
+      className={`flex w-full p-2 justify-center items-center h-36 hover:bg-red-800/50 ${
+        index % 2 === 0 ? "bg-black/40" : ""
+      } animated-div`}
+    >
+      <div className="w-1/3 flex flex-col justify-center items-center text-center">
+        <Image
+          src={match.teams.home.logo}
+          alt="HomeLogo"
+          width={70}
+          height={70}
+        />
+        {match.teams.home.name}
+      </div>
+      <div className="w-1/3 flex flex-col justify-center items-center h-full">
+        <div className="h-1/3 text-xs text-center">
+          <LocalTime fixture={match} />
+        </div>
+        <div className="h-1/3 text-center">vs</div>
+        <div className="h-1/3"></div>
+      </div>
+      <div className="w-1/3 flex flex-col justify-center items-center text-center">
+        <Image
+          src={match.teams.away.logo}
+          alt="AwayLogo"
+          width={70}
+          height={70}
+          title="match logo"
+        />
+        {match.teams.away.name}
+      </div>
+    </Link>
+  ) : null;
+};
+
+export default FixtureItem;
