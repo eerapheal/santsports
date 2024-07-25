2b08ad5a7c4a47aec7e3d0946ced10d932e52e40
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-26 00:43:22 +0530

    modified

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -5,22 +5,20 @@ import { AllFixtures, Standing } from "@/utils/type";
 import getStandings from "@/utils/getApis/getStanding";
 import Loading from "./loading";
 import getFixturesForFiveLeague from "@/utils/getApis/getFixturesForFiveLeague";
-import  StandingFixtures from "@/components/home/standing.fixtures"
-
+import StandingFixtures from "@/components/home/standing.fixtures"
 
 export const metadata: Metadata = {
   title:
     process.env.NEXT_PUBLIC_APP_NAME ||
-    "SantSport live sports scores like never before",
+    "SantSport | Sports live scores, standing tables and upcoming fixtures and match",
   description:
     process.env.NEXT_PUBLIC_APP_DESC ||
-    "Experience the latest live sports scores like never before with Sant Sport. We are committed to providing you with all the trending sports news and real-time scores on the go.",
+    "Experience the latest live sports scores like never before with Sant Sport. We are committed to providing you with all the trending sports news and real-time scores, tables, standing, upcoming matches, results and  form on the go.",
 };
 
 const HomePage = async () => {
   const standingData: Standing[] = await getStandings();
   const filteredFixtures: AllFixtures[] = await getFixturesForFiveLeague();
-  console.log(filteredFixtures);
 
   return (
     <div className="flex flex-col justify-center items-center w-full md:p-10">
