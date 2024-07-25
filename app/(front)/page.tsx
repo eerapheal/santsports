f0cf7151723ecf1d121207a8b3c20f415924ae68
Ekpenisi E Raphael <ekpenisiraphael@gmail.com>
2024-07-26 01:34:48 +0530

    add to fix bug

---
+++ app/(front)/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+import { AllFixtures, Standing } from "@/utils/type";
+import getStandings from "@/utils/getApis/getStanding";
+import Loading from "./loading";
+import getFixturesForFiveLeague from "@/utils/getApis/getFixturesForFiveLeague";
+simport StandingFixtures from "@/components/home/standing.fixtures"
+
+export const metadata: Metadata = {
+  title:
+    process.env.NEXT_PUBLIC_APP_NAME ||
+    "SantSport | Sports live scores, standing tables and upcoming fixtures and match",
+  description:
+    process.env.NEXT_PUBLIC_APP_DESC ||
+    "Experience the latest live sports scores like never before with Sant Sport. We are committed to providing you with all the trending sports news and real-time scores, tables, standing, upcoming matches, results and  form on the go.",
+};
+
+const HomePage = async () => {
+  const standingData: Standing[] = await getStandings();
+  const filteredFixtures: AllFixtures[] = await getFixturesForFiveLeague();
+
+  return (
+    <div className="flex flex-col justify-center items-center w-full md:p-10">
+      <Suspense fallback={<Loading />}>
+        <StandingFixtures
+          standingData={standingData}
+          filteredFixtures={filteredFixtures}
+        />
+      </Suspense>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ components/loading.components.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingComponents = () => {
+  return (
+    <div>LoadingComponents</div>
+  )
+}
+
+export default LoadingComponents;
